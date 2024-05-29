b86f8f6335d08cf23b15129944504b0139ac3ff7
Shailendra Kumar <emailtoshailendra@gmal.com>
2024-05-30 00:58:41 +0530

    Adding templates and static files

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.homepage),
+    path('about/',views.about),
+
 ]
